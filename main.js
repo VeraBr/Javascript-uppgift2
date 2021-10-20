d7429d6ebea5e33c2e9c08153856fe19dddce2c6
vera-braathen <vera.braathen@hotmail.com>
2021-10-20 14:37:05 +0200

    Changes in main.js and index.html

--- main.js
+++ main.js
@@ -1,41 +1,30 @@
+// Variabler för alla element i HTML filen.
 let queueButton = document.querySelector("#standInLine");
 let fastTrackButton = document.querySelector("#fastTrack");
 let checkInButton = document.querySelector("#checkedIn")
 let listContainer = document.querySelector("#container");
 let inputValue = document.querySelector("#inputName");
 
+// Skapar incheckningskö-rubriken.
 let checkInHeader = document.createElement("h2");
 checkInHeader.innerText = "Incheckningskö";
 listContainer.appendChild(checkInHeader);
 
-
+// Skapar ol element för incheckningskön.
 let numberList = document.createElement("ol");
-numberList.setAttribute("id", "lista");
 listContainer.appendChild(numberList);
 
-let queueArray = [];
-let queueList2 = document.getElementById("lista");
+// Skriver ut att kön är tom från början.
+let empty = document.createElement("p");
+empty.innerText = "Just nu står ingen person i kö.";
+checkInHeader.appendChild(empty);
 
-if (queueArray.length === 0){
-    let empty = document.createElement("p");
-    empty.innerText = "There’s currently no people standing in line";
-    checkInHeader.appendChild(empty);
-}
 
 queueButton.addEventListener("click",()=> {
-    if (document.getElementById("inputName").value.length == 0) {
-        alert("Empty");
-    } else if (checkInHeader.children.length > 0) {
-        emptyElement = document.querySelector("p");
-        checkInHeader.removeChild(emptyElement);
-        queueArray.push(inputValue.value);
-        let queueList = document.createElement("li");
-        queueList.innerText = inputValue.value;
-        numberList.appendChild(queueList);
-        inputValue.value = "";
-    }
-        else {
-        queueArray.push(inputValue.value);
+    if (!document.getElementById("inputName").value.trim().length) { //Undviker att inputfältet lämnas tomt.
+        alert("Rutan får inte lämnas tom");
+    } else {
+        empty.innerText = "";
         let queueList = document.createElement("li");
         queueList.innerText = inputValue.value;
         numberList.appendChild(queueList);
@@ -44,18 +33,10 @@ queueButton.addEventListener("click",()=> {
 })
 
 fastTrackButton.addEventListener("click", ()=> {
-    if (document.getElementById("inputName").value.length == 0) {
-        alert("Empty");
-    } else if (checkInHeader.children.length > 0) {
-        emptyElement = document.querySelector("p");
-        checkInHeader.removeChild(emptyElement);
-        queueArray.unshift(inputValue.value);
-        let queueList = document.createElement("li");
-        queueList.innerText = inputValue.value;
-        numberList.prepend(queueList);
-        inputValue.value = "";
+    if (!document.getElementById("inputName").value.trim().length) {
+        alert("Rutan får inte lämnas tom");
     } else {
-        queueArray.unshift(inputValue.value);
+        empty.innerText = "";
         let queueList = document.createElement("li");
         queueList.innerText = inputValue.value;
         numberList.prepend(queueList);
@@ -64,12 +45,9 @@ fastTrackButton.addEventListener("click", ()=> {
 })
 
 checkInButton.addEventListener("click", ()=> {
-    queueArray.shift();
     let firstElement = document.querySelector("li")
     numberList.removeChild(firstElement);
-    if (queueArray.length === 0){
-        let empty = document.createElement("p");
-        empty.innerText = "There’s currently no people standing in line";
-        checkInHeader.appendChild(empty);
+    if (numberList.children.length === 0){
+        empty.innerText = "Just nu står ingen person i kö.";
     }
 })
